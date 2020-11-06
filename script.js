616dced540f69db320a317e145a58569f21fd958
Julie P <paupertjulie@gmail.com>
2020-11-06 14:50:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//Selectors
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+//Event listener pour ouvrir le menu
+hamburgerMenu.addEventListener('click', function() {
+    header.classList.toggle('menu-open');
+});
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".header").css("background", "var(--red-alt)");
+          $(".header").css("opacity", ".9");
+        }
+        else{
+            $(".header").css("background" , "transparent");  	
+        }
+    })
+})
